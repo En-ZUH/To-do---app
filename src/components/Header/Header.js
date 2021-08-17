087695587a008ba9b4193aa8b92a8b22621e2b00
En-ZUH <enas_z_bataineh108@yahoo.com>
2021-08-17 16:19:39 +0300

    done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+// import React from 'react'
+
+// export class Header extends React.Component {
+
+//     constructor(props) {
+//         super(props)
+
+//         this.state = {
+//             incomplete: props.incomplete
+//         }
+//     }
+//     render() {
+//         return (
+//             <Header>
+//                 <h1>To Do List: {this.state.incomplete} items pending</h1>
+//                 {/* <h3> incomplete={this.state.incomplete}</h3> */}
+//             </Header>
+//         )
+//     }
+// }
+
+// export default Header;
